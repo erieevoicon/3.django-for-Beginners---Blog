7d87bbc18dd38682b531835d777d7521e243a3ce
erieevoicon <thanhtung161094@gmail.com>
2022-09-06 14:32:14 +0700

    forms and user accounts

--- django_project/settings.py
+++ django_project/settings.py
@@ -27,7 +27,8 @@ DEBUG = True
 
 ALLOWED_HOSTS = []
 
-
+LOGIN_REDIRECT_URL = 'home'
+LOGOUT_REDIRECT_URL = 'home'
 # Application definition
 
 INSTALLED_APPS = [
@@ -38,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'blog.apps.BlogConfig',
+    'accounts.apps.AccountsConfig',
 ]
 
 MIDDLEWARE = [
